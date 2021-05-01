e99fb4194052655df6305795c020c53a22eb1062
bella <tianqibu@hotmail.com>
2021-05-01 15:03:54 +0100

    Make home page responsive

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import Impact from '../images/impact.png'
 import Resources from '../images/resources-yellow.png'
+import { Link } from 'react-router-dom'
 
 import './Home.css'
 
@@ -9,7 +10,7 @@ const Home = () => {
             <div className="about-us">
                 <h1>SUSTAINABLE REWARDS FOR GREEN LIVING</h1>
                 <p>A rewards program to help you become more sustainable and minimise your impact on the world. Earn points based on eco-friendly actions you've undertaken to spend on real rewards including vouchers and 4-night retreats.</p>
-                <button>Sign Up</button>
+                <Link to="/sign-up"><button>Sign Up</button></Link>
             </div>
             <img 
                 src={Impact} 
