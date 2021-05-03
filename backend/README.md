a79b6dc9c7d45a1e87491770678303d4ebf2da07
annamariacutolo <52924438+annamariacutolo@users.noreply.github.com>
2021-05-03 15:17:51 +0100

    create models for db and routes for app

---
+++ backend/README.md
@@ -0,0 +1,13 @@
+## the green incentive flask backend
+endpoints:
+'/'
+'/signin'
+'/signup'
+'/impact'
+'/resources'
+
+--users only--
+'/dashboard'
+'/activities'
+'/rewards'
+'/garden'
---
+++ backend/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify, session, flash, redirect, url_for, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_login import current_user, login_user, logout_user, login_required, LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./thegreenincentive.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '4c4dfac39f8c6d672d41928d89545c74'
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+login = LoginManager(app)
+login.login_view = 'login'
+from models import User, Activity, Reward
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({ 'home': 'home page' })
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    else: 
+        return jsonify({ 'login': 'login page' })
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    else:
+        return jsonify({ 'register': 'register page' })
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/impact')
+def impact():
+    return jsonify({ 'impact': 'impact page' })
+
+@app.route('/resources')
+def resources():
+    return jsonify({ 'resources': 'resources page' })
+
+@login_required
+@app.route('/dashboard')
+def dashboard():
+    return jsonify({ 'dashboard': 'dashboard page' })
+
+@login_required
+@app.route('/activities')
+def activities():
+    return jsonify({ 'activities': 'activities page' })
+
+@login_required
+@app.route('/rewards')
+def rewards():
+    return jsonify({ 'rewards': 'rewards page' })
+
+@login_required
+@app.route('/garden')
+def garden():
+    return jsonify({ 'garden': 'garden page' })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
