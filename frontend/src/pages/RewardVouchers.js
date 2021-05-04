981827aa0b40360c382885cfdde0f8664785bd13
Mahbub55 <78920368+Mahbub55@users.noreply.github.com>
2021-05-04 01:20:27 +0100

    Added RewardMain and RewardVoucher pages

---
+++ frontend/src/pages/RewardVouchers.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Impact from '../images/impact.png'
+
+import RewardVouchersTitle from '../components/RewardVouchers/RewardVouchersTitle.js'
+
+import './RewardMain.css'
+
+const RewardVouchers = () => {
+    return (
+        <div>
+            <RewardVouchersTitle />
+            <div className="home-container">
+            <div className="flex-parent-rm">
+                    <div className="flex-child-rm">
+                        <Link to='#'>
+                            <img className="category-img" src={Impact} alt=''></img>
+                            <div className="category-text">20% off sustainable gloss</div>
+                        </Link>
+                    </div>
+                    <div className="flex-child-rm">
+                        <Link to='#'>
+                            <img className="category-img" src={Impact} alt='' text-decoration="Exclusive Offers"></img>
+                            <div className="category-text">30% off juicer from tefal</div>
+                        </Link>
+                    </div>
+            </div>
+        </div>
+    </div>
+        
+    )
+}
+
+export default RewardVouchers
