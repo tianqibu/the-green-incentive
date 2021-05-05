03298e57a7c821367a2c2562b713a873e7460fee
bella <tianqibu@hotmail.com>
2021-05-05 12:22:48 +0100

    connect dashboard and garden to backend

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,40 @@
 import Title from '../../components/Title/Title'
 import DashboardImages from '../../components/DashboardImages/DashboardImages'
 import ProgressBar from '../../components/ProgressBar/ProgressBar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './Dashboard.css'
 
 const Dashboard = () => {
+
+    const [ userDetails, setUserDetails ] = useState({
+        username: '',
+        email: '',
+        id: '',
+        points: ''
+    })
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            const res = await fetch('/api/users/current', {
+              method: 'GET',
+            })
+    
+            const data = await res.json()
+
+            setUserDetails({
+                username: data.username,
+                email: data.email,
+                id: data.id,
+                points: data.points
+            })
+           
+            return data
+          }
+
+          fetchUserDetails()
+    
+    }, [])
 
     const [percentage, setPercentage] = useState(60)
 
@@ -16,13 +45,13 @@ const Dashboard = () => {
             </div>
             <div className="points">
                 <p className="bold">Points balance</p>
-                <p>1600</p>
+                <p>{userDetails.points}</p>
             </div>
             <div className="progress">
                 <p className="bold">Daily goal progress</p>
                 <ProgressBar percentage={percentage} />
             </div>
-            <DashboardImages />
+            <DashboardImages/>
         </div>
     )
 }
